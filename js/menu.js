88cad93bede2b19a5a5d4d5ec1d94c2f43e8a12e
David1100 <acevdo22@hotmail.com>
2018-03-09 13:48:11 -0500

    primer projecto

---
+++ js/menu.js
@@ -0,0 +1,59 @@
+var btnMenu = document.getElementById("btn-menu");
+var nav = document.getElementById("nav");
+var slider=$('#slider');
+var anterior=$('#btn-prev');
+var siguiente=$('#btn-next');
+
+
+btnMenu.addEventListener('click', function(){
+nav.classList.toggle('mostrar');
+});
+
+
+
+/*--------------slider---------------------*/
+
+$('#slider li:last').insertBefore('#slider li:first');
+
+slider.css('margin-left', '-'+100+'%');
+
+function moverD(){
+   slider.animate({marginLeft:'-'+200+'%'},800,function(){
+      $('#slider li:first').insertAfter('#slider li:last');
+       slider.css('margin-left', '-'+100+'%');
+   }); 
+}
+
+function moverI(){
+   slider.animate({marginLeft:0},800,function(){
+      $('#slider li:last').insertBefore('#slider li:first');
+       slider.css('margin-left', '-'+100+'%');
+   }); 
+}
+
+siguiente.on('click', function(){
+             moverD();
+             });
+anterior.on('click', function(){
+             moverI();
+             });
+
+/*--------------scroll---------------------*/
+
+$('#inicio').on('click', function(){
+    $('html,boy').animate({scrollTop:0}, 1500);
+})
+$('#nosotros').on('click', function(){
+    $('html,boy').animate({scrollTop:614}, 1500);
+})
+$('#nosotros2').on('click', function(){
+    $('html,boy').animate({scrollTop:614}, 1500);
+})
+$('#catalogo').on('click', function(){
+    $('html,boy').animate({scrollTop:1228}, 1500);
+})
+$('#catalogo2').on('click', function(){
+    $('html,boy').animate({scrollTop:1228}, 1500);
+})
+
+
